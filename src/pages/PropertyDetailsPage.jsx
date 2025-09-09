2b0f703b6e08ffefe912bd8bdb3e85f5c5030eb7
julzedz <edoziechukwuma@gmail.com>
2025-09-09 17:13:45 +0100

    Update label on PropertyDetailsPage page.

--- src/pages/PropertyDetailsPage.jsx
+++ src/pages/PropertyDetailsPage.jsx
@@ -115,7 +115,7 @@ const PropertyDetailsPage = () => {
               <b>Bathrooms:</b> {attr.bathrooms}
             </Text>
             <Text>
-              <b>Social Media Video:</b>{" "}
+              <b>Click for more images/videos:</b>{" "}
               <ChakraLink
                 href={attr.instagram_video_link}
                 color="purple.600"
